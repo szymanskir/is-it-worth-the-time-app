258a80281c4cf5e52457b4daae3f2a6f33d0cc5f
szymanskir <ryszard.szymanski@outlook.com>
2025-04-13 21:32:31 +0200

    WIP - add xkcd comic test cases

--- src/calculator/calculations.test.ts
+++ src/calculator/calculations.test.ts
@@ -22,9 +22,33 @@ describe('calculatePotentialSavedTime', () => {
         ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Week}   | ${1}         | ${TimeUnit.Second}  | ${260}               | ${TimeUnit.Second}
         ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Day}    | ${1}         | ${TimeUnit.Second}  | ${1825}              | ${TimeUnit.Second}
         ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Year}   | ${1}         | ${TimeUnit.Day}     | ${5 * 24 * 60 * 60}  | ${TimeUnit.Second}
-        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Month}  | ${1}         | ${TimeUnit.Day}     | ${8}                 | ${TimeUnit.Week}
+
+        // xkcd comic test cases (row wise)
+        ${5}         | ${TimeUnit.Year} | ${50}          | ${TimeUnit.Day}    | ${1}         | ${TimeUnit.Second}  | ${1}                 | ${TimeUnit.Day}
+        ${5}         | ${TimeUnit.Year} | ${5}           | ${TimeUnit.Day}    | ${1}         | ${TimeUnit.Second}  | ${2}                 | ${TimeUnit.Hour}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Day}    | ${1}         | ${TimeUnit.Second}  | ${30}                | ${TimeUnit.Minute}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Week}   | ${1}         | ${TimeUnit.Second}  | ${4}                 | ${TimeUnit.Minute}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Month}  | ${1}         | ${TimeUnit.Second}  | ${1}                 | ${TimeUnit.Minute}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Year}   | ${1}         | ${TimeUnit.Second}  | ${5}                 | ${TimeUnit.Second}
+
+        ${5}         | ${TimeUnit.Year} | ${50}          | ${TimeUnit.Day}    | ${5}         | ${TimeUnit.Second}  | ${5}                 | ${TimeUnit.Day}
+        ${5}         | ${TimeUnit.Year} | ${5}           | ${TimeUnit.Day}    | ${5}         | ${TimeUnit.Second}  | ${12}                | ${TimeUnit.Hour}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Day}    | ${5}         | ${TimeUnit.Second}  | ${2}                 | ${TimeUnit.Hour}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Week}   | ${5}         | ${TimeUnit.Second}  | ${21}                | ${TimeUnit.Minute}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Month}  | ${5}         | ${TimeUnit.Second}  | ${5}                 | ${TimeUnit.Minute}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Year}   | ${5}         | ${TimeUnit.Second}  | ${25}                | ${TimeUnit.Second}
+
+        ${5}         | ${TimeUnit.Year} | ${50}          | ${TimeUnit.Day}    | ${30}        | ${TimeUnit.Second}  | ${4}                 | ${TimeUnit.Week}
+        ${5}         | ${TimeUnit.Year} | ${5}           | ${TimeUnit.Day}    | ${30}        | ${TimeUnit.Second}  | ${3}                 | ${TimeUnit.Day}
+        // Skipped (to be analyzed)
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Week}   | ${30}        | ${TimeUnit.Second}  | ${2}                 | ${TimeUnit.Hour}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Month}  | ${30}        | ${TimeUnit.Second}  | ${30}                | ${TimeUnit.Minute}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Year}   | ${30}        | ${TimeUnit.Second}  | ${2}                 | ${TimeUnit.Minute}
+
         ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Month}  | ${6}         | ${TimeUnit.Hour}    | ${2}                 | ${TimeUnit.Week}
-    `(`should return $expected seconds for a $taskDuration $taskDurationUnit 
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Month}  | ${1}         | ${TimeUnit.Day}     | ${8}                 | ${TimeUnit.Week}
+        ${5}         | ${TimeUnit.Year} | ${1}           | ${TimeUnit.Year}   | ${1}         | ${TimeUnit.Day}     | ${5}                 | ${TimeUnit.Day}
+    `(`should return $expected $expectedUnit for a $taskDuration $taskDurationUnit 
         task done $frequencyValue per $frequencyUnit for $horizonValue $horizonUnit`, (calculationScenario: CalculationScenario) => {
         const { expected, expectedUnit, ...calculationInput } = calculationScenario;
         const result = calculatePotentialSavedTime(calculationInput, expectedUnit);
