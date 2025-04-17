4f7bd871417b6fb5c3493eea8ef277839c1ca92c
szymanskir <ryszard.szymanski@outlook.com>
2025-04-17 22:47:30 +0200

    feat: display initial time saved in days

--- src/calculator/Calculator.tsx
+++ src/calculator/Calculator.tsx
@@ -2,115 +2,138 @@ import { useState } from "react";
 import { Button } from "../components/ui/button";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "../components/ui/select";
 import { TimeCalculationInput, TimeUnit } from "./TimeCalculationInput";
+import { calculatePotentialSavedTime } from "./calculations";
 
 function Calculator() {
   const [timeCalculationInput, setTimeCalculationInput] = useState<TimeCalculationInput>({
-    horizonValue: 1,
-    horizonUnit: TimeUnit.Day,
+    horizonValue: 5,
+    horizonUnit: TimeUnit.Year,
     frequencyValue: 1,
     frequencyUnit: TimeUnit.Day,
-    taskDuration: 1,
+    taskDuration: 15,
     taskDurationUnit: TimeUnit.Minute,
   });
+
+  const [timeSavingResult, setTimeSavingResult] = useState<number | null>(calculatePotentialSavedTime(timeCalculationInput, TimeUnit.Day));
 
   const handleInputChange = (field: keyof TimeCalculationInput, value: string | number) => {
     setTimeCalculationInput((prev) => ({
       ...prev,
       [field]: value,
     }));
-  };
 
-  const handleSubmit = () => {
-    console.log("TimeCalculationInput:", timeCalculationInput);
   };
 
   return (
-    <div className="calculator">
-      <div className="input-group flex justify-items-start items-center gap-4 mb-4">
-        <label htmlFor="horizon-value" className="w-2xs text-left">How long will you keep doing it?</label>
-        <input
-          id="horizon-value"
-          type="number"
-          min="1"
-          placeholder="1"
-          className="input border rounded px-2 py-1 w-32"
-          value={timeCalculationInput.horizonValue}
-          onChange={(e) => handleInputChange("horizonValue", e.target.value)}
-        />
-        <Select
-          onValueChange={(value) => handleInputChange("horizonUnit", value as TimeUnit)}
-          value={timeCalculationInput.horizonUnit}
-        >
-          <SelectTrigger className="w-44 border rounded px-2 py-1">
-            <SelectValue placeholder="Unit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value={TimeUnit.Day}>Days</SelectItem>
-            <SelectItem value={TimeUnit.Week}>Weeks</SelectItem>
-            <SelectItem value={TimeUnit.Month}>Months</SelectItem>
-            <SelectItem value={TimeUnit.Year}>Years</SelectItem>
-          </SelectContent>
-        </Select>
+    <div className="calculator flex flex-col items-center gap-6">
+      <div className="card border rounded shadow p-6">
+        <div className="card-body">
+          <div className="input-group flex justify-items-start items-center gap-4 mb-4">
+            <div className="flex flex-col items-start">
+              <label htmlFor="horizon-value" className="mb-1 text-left font-semibold">How long will you keep doing it?</label>
+              <div className="flex items-center gap-4">
+                <input
+                  id="horizon-value"
+                  type="number"
+                  min="1"
+                  placeholder="1"
+                  className="input border rounded px-2 py-1 w-32"
+                  value={timeCalculationInput.horizonValue}
+                  onChange={(e) => handleInputChange("horizonValue", e.target.value)}
+                />
+                <Select
+                  onValueChange={(value) => handleInputChange("horizonUnit", value as TimeUnit)}
+                  value={timeCalculationInput.horizonUnit}
+                >
+                  <SelectTrigger className="w-44 border rounded px-2 py-1">
+                    <SelectValue placeholder="Unit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value={TimeUnit.Day}>Days</SelectItem>
+                    <SelectItem value={TimeUnit.Week}>Weeks</SelectItem>
+                    <SelectItem value={TimeUnit.Month}>Months</SelectItem>
+                    <SelectItem value={TimeUnit.Year}>Years</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+
+          <div className="input-group flex justify-items-start items-center gap-4 mb-4">
+            <div className="flex flex-col items-start">
+              <label htmlFor="frequency-value" className="mb-1 text-left font-semibold">How often do you do it?</label>
+              <div className="flex items-center gap-4">
+                <input
+                  id="frequency-value"
+                  type="number"
+                  min="1"
+                  placeholder="1"
+                  className="input border rounded px-2 py-1 w-32"
+                  value={timeCalculationInput.frequencyValue}
+                  onChange={(e) => handleInputChange("frequencyValue", e.target.value)}
+                />
+                <Select
+                  onValueChange={(value) => handleInputChange("frequencyUnit", value as TimeUnit)}
+                  value={timeCalculationInput.frequencyUnit}
+                >
+                  <SelectTrigger className="w-44 border rounded px-2 py-1">
+                    <SelectValue placeholder="Unit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value={TimeUnit.Day}>Per Day</SelectItem>
+                    <SelectItem value={TimeUnit.Week}>Per Week</SelectItem>
+                    <SelectItem value={TimeUnit.Month}>Per Month</SelectItem>
+                    <SelectItem value={TimeUnit.Year}>Per Year</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+
+          <div className="input-group flex items-center gap-4 mb-4">
+            <div className="flex flex-col items-start">
+              <label htmlFor="task-duration" className="mb-1 text-left font-semibold">How long does it take?</label>
+              <div className="flex items-center gap-4">
+
+                <input
+                  id="task-duration"
+                  type="number"
+                  min="1"
+                  placeholder="15"
+                  className="input border rounded px-2 py-1 w-32"
+                  value={timeCalculationInput.taskDuration}
+                  onChange={(e) => handleInputChange("taskDuration", e.target.value)}
+                />
+                <Select
+                  onValueChange={(value) => handleInputChange("taskDurationUnit", value as TimeUnit)}
+                  value={timeCalculationInput.taskDurationUnit}
+                >
+                  <SelectTrigger className="w-44 border rounded px-2 py-1">
+                    <SelectValue placeholder="Unit" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value={TimeUnit.Second}>Seconds</SelectItem>
+                    <SelectItem value={TimeUnit.Minute}>Minutes</SelectItem>
+                    <SelectItem value={TimeUnit.Hour}>Hours</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
 
-      <div className="input-group flex justify-items-start items-center gap-4 mb-4">
-        <label htmlFor="frequency-value" className="w-2xs text-left">How often do you do it?</label>
-        <input
-          id="frequency-value"
-          type="number"
-          min="1"
-          placeholder="1"
-          className="input border rounded px-2 py-1 w-32"
-          value={timeCalculationInput.frequencyValue}
-          onChange={(e) => handleInputChange("frequencyValue", e.target.value)}
-        />
-        <Select
-          onValueChange={(value) => handleInputChange("frequencyUnit", value as TimeUnit)}
-          value={timeCalculationInput.frequencyUnit}
-        >
-          <SelectTrigger className="w-44 border rounded px-2 py-1">
-            <SelectValue placeholder="Unit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value={TimeUnit.Day}>Per Day</SelectItem>
-            <SelectItem value={TimeUnit.Week}>Per Week</SelectItem>
-            <SelectItem value={TimeUnit.Month}>Per Month</SelectItem>
-            <SelectItem value={TimeUnit.Year}>Per Year</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-
-      <div className="input-group flex items-center gap-4 mb-4">
-        <label htmlFor="task-duration" className="w-2xs text-left">How long does it take?</label>
-        <input
-          id="task-duration"
-          type="number"
-          min="1"
-          placeholder="15"
-          className="input border rounded px-2 py-1 w-32"
-          value={timeCalculationInput.taskDuration}
-          onChange={(e) => handleInputChange("taskDuration", e.target.value)}
-        />
-        <Select
-          onValueChange={(value) => handleInputChange("taskDurationUnit", value as TimeUnit)}
-          value={timeCalculationInput.taskDurationUnit}
-        >
-          <SelectTrigger className="w-44 border rounded px-2 py-1">
-            <SelectValue placeholder="Unit" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value={TimeUnit.Second}>Seconds</SelectItem>
-            <SelectItem value={TimeUnit.Minute}>Minutes</SelectItem>
-            <SelectItem value={TimeUnit.Hour}>Hours</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="flex items-right justify-end gap-4 mb-4">
-        <Button variant="outline" onClick={handleSubmit}>Calculate</Button>
+      <div className="card border rounded shadow p-4 w-64">
+        <div className="card-header text-lg font-bold">Time Saved</div>
+        <div className="card-body text-center text-2xl font-semibold">
+          {`${calculatePotentialSavedTime(timeCalculationInput, TimeUnit.Day)} ${TimeUnit.Day}`}
+          <div className="text-sm text-gray-500 mt-2">
+            over {timeCalculationInput.horizonValue} {timeCalculationInput.horizonUnit}(s).
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
 
 export default Calculator;
