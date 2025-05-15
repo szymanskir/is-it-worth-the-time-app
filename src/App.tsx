cdced7ae23422b7bd6bb5d7c42d81bb09b393634
szymanskir <ryszard.szymanski@outlook.com>
2025-05-15 19:15:13 +0200

    ui: improve mobile layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import Calculator from "./calculator/Calculator";
 
 function App() {
   return (
-    <div className="app flex flex-col items-center gap-6 p-4">
-      <header className="flex items-center gap-2 text-2xl font-bold">
+    <div className="p-4 app flex flex-col items-center gap-6 ">
+      <header className="text-xl font-bold">
         <h1>Is It Worth The Time?</h1>
       </header>
-
-      <Calculator />
+      <div className="w-full max-w-[360px]">
+        <Calculator />
+      </div>
     </div>
   );
 }
